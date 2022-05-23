332c7a80b843f367e57ed28235ab4cd33c5b3568
Ali BAGDATLI <alibagdat@hotmail.com>
2022-05-23 12:38:56 +0200

    .

--- SShot.py
+++ SShot.py
@@ -11,7 +11,7 @@ month=1
 print('Press Ctrl-C to quit.')
 
 time.sleep(5)
-path='C:/Users/AliBagdatli/Videos/Captures/dayByday'  #+str(year)
+path='./Captures/dayByday'  #+str(year)
 folder=path+'/'
 try:
     os.mkdir(path)
@@ -22,16 +22,11 @@ else:
 
 try:
     for month in range(323,350):
-        
-        # positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-        # print(positionStr, end='')
-        # print('\b' * len(positionStr), end='', flush=True)
+
         pyautogui.moveTo(1170, 110, duration=0.25)
         x, y = pyautogui.position()
         pyautogui.click(x,y)
-        # time.sleep(3)
-        # pyautogui.moveTo(90, 1010, duration=0.25)
-        # x, y = pyautogui.position()
+
         time.sleep(2)
         dosya=folder+str(month)+'.jpg'
         im2 = pyautogui.screenshot(dosya,region=(0,0, 1920, 1025))
